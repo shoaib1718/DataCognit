0c9d95768717acc0815010cadfb115a8b05227cd
Shoaib Janvekar <85121205+shoaib1718@users.noreply.github.com>
2024-09-21 03:06:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -23,5 +23,26 @@ const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('nav ul');
 
 hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
   navMenu.classList.toggle('active');
+});
+
+// Show the scroll button when the user scrolls down 20px from the top
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  const scrollToTopButton = document.getElementById("scrollToTop");
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollToTopButton.style.display = "block";
+  } else {
+    scrollToTopButton.style.display = "none";
+  }
+}
+
+// When the user clicks the button, scroll to the top
+document.getElementById("scrollToTop").addEventListener("click", function() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
 });
